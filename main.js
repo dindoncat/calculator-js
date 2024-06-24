f8e6fdaf8fdfded10d91bf2b591b47ef6e514048
dindoncat <yisraeldbenisti@gmail.com>
2024-06-24 14:10:01 +0300

    i think i'm done!

--- main.js
+++ main.js
@@ -1,136 +1,115 @@
 let numbers = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0, ".", "( )"];
-let mainSrt = "0";
+let mainStr = "0";
 let secStr = "";
 let continuez = false;
 
-const roundToSixDec = (strNum) => {
-    let num = strNum;
-    num = Number(num);
-    console.log(num , typeof num , num == strNum);
-    num = Math.round(num * Math.pow(10, 6)) / Math.pow(10, 6);
-    return num.toString();
-};
-document.querySelector("#sum").innerHTML = mainSrt;
+document.querySelector("#sum").innerHTML = mainStr;
 
-let btnDiv = document.querySelector("#numbers_btns");
+const roundToSixDec = (strNum) => {
+  let num = Number(strNum);
+  return (Math.round(num * 1e6) / 1e6).toString();
+};
 
 const operatorsCheck = () => {
-  if (
-    mainSrt[mainSrt.length - 1] == "/" ||
-    mainSrt[mainSrt.length - 1] == "*" ||
-    mainSrt[mainSrt.length - 1] == "-" ||
-    mainSrt[mainSrt.length - 1] == "+"
-  ) {
-    mainSrt = mainSrt.slice(0, -1);
+  const operators = ["/", "*", "-", "+", "."];
+  if (operators.includes(mainStr[mainStr.length - 1])) {
+    mainStr = mainStr.slice(0, -1);
   }
+};
+const applyOperator = (operator) => {
+  operatorsCheck();
+  mainStr += operator;
+  document.querySelector("#sum").innerHTML = mainStr;
+  continuez = false;
+};
+const divideOperator = () => applyOperator("/");
+const multiplyOperator = () => applyOperator("*");
+const minusOperator = () => applyOperator("-");
+const plusOperator = () => applyOperator("+");
 
-};
-const divideOperator = () => {
-  operatorsCheck();
-  mainSrt += "/";
-  document.querySelector("#sum").innerHTML = mainSrt;
-  continuez = false;
-};
-const multipleOperator = () => {
-  operatorsCheck();
-  mainSrt += "*";
-  document.querySelector("#sum").innerHTML = mainSrt;
-  continuez = false;
-};
-const minusOperator = () => {
-  operatorsCheck();
-  mainSrt += "-";
-  document.querySelector("#sum").innerHTML = mainSrt;
-  continuez = false;
-};
-const plusOperator = () => {
-  operatorsCheck();
-  mainSrt += "+";
-  document.querySelector("#sum").innerHTML = mainSrt;
-  continuez = false;
-};
 const erase = () => {
-  mainSrt = mainSrt.slice(0, -1);
-  if (mainSrt.length == 0) {
-    mainSrt = "0";
+  mainStr = mainStr.slice(0, -1);
+  if (mainStr.length === 0) {
+    mainStr = "0";
   }
-  document.querySelector("#sum").innerHTML = mainSrt;
+  document.querySelector("#sum").innerHTML = mainStr;
 };
+
 const equal = () => {
-  secStr = mainSrt;
+  if (isNaN(Number(mainStr[mainStr.length - 1])))
+    mainStr = mainStr.substring(0, mainStr.length - 1);
+  secStr = mainStr;
   document.querySelector("#calculation").innerHTML = secStr;
-  let result = eval(mainSrt);
-  console.log(result);
-  mainSrt = roundToSixDec(result);
-  document.querySelector("#sum").innerHTML = mainSrt;
+  let result = eval(mainStr);
+  mainStr = roundToSixDec(result);
+  document.querySelector("#sum").innerHTML = mainStr;
   continuez = true;
 };
+
 numbers.forEach((number) => {
+  let btnDiv = document.querySelector("#numbers_btns");
   let btn = document.createElement("button");
   btn.innerHTML = number;
   btnDiv.appendChild(btn);
   btn.addEventListener("click", () => {
     if (continuez) {
-      mainSrt = "0";
+      mainStr = "0";
       continuez = false;
     }
-    if (number != "( )") mainSrt += number;
-    if (mainSrt[0] == "0" && mainSrt[1] != ".") {
-      mainSrt = mainSrt.slice(1);
+    if (
+      (Number(number) || number === "0") &&
+      mainStr[mainStr.length - 1] !== ")"
+    ) {
+      if (mainStr[0] === "0") mainStr = "";
+      mainStr += number;
     } else if (number === "( )") {
-      if (mainSrt[0] == "0") {
-        mainSrt = "(";
-      }
-      if (mainSrt.split("(").length > mainSrt.split(")").length) {
-        mainSrt += ")";
+      if(mainStr === "0") mainStr = "";
+      operatorsCheck()
+      if (mainStr.split("(").length > mainStr.split(")").length) {
+        mainStr += ")";
       } else {
-        mainSrt += "(";
+        mainStr += "(";
       }
+    } else if (number === ".") {
+      //if thers a dot to nothing add a zero to it
+      operatorsCheck();
+      if (mainStr[0] === "") mainStr = "0" + mainStr;
+      mainStr += number;
     }
-    document.querySelector("#sum").innerHTML = mainSrt;
+    document.querySelector("#sum").innerHTML = mainStr;
   });
 });
-document.querySelector("#claer").addEventListener("click", () => {
-  mainSrt = "0";
+
+document.querySelector("#clear").addEventListener("click", () => {
+  mainStr = "0";
   secStr = "";
-  document.querySelector("#sum").innerHTML = mainSrt;
+  document.querySelector("#sum").innerHTML = mainStr;
   document.querySelector("#calculation").innerHTML = secStr;
 });
-document.querySelector("#erase").addEventListener("click", () => erase());
-document
-  .querySelector("#divide")
-  .addEventListener("click", () => divideOperator());
-document
-  .querySelector("#multiply")
-  .addEventListener("click", () => multipleOperator());
-document
-  .querySelector("#subtract")
-  .addEventListener("click", () => minusOperator());
-document.querySelector("#add").addEventListener("click", () => plusOperator());
-document.querySelector("#equal").addEventListener("click", () => equal());
+document.querySelector("#erase").addEventListener("click", erase);
+document.querySelector("#divide").addEventListener("click", divideOperator);
+document.querySelector("#multiply").addEventListener("click", multiplyOperator);
+document.querySelector("#subtract").addEventListener("click", minusOperator);
+document.querySelector("#add").addEventListener("click", plusOperator);
+document.querySelector("#equal").addEventListener("click", equal);
 
 addEventListener("keydown", (e) => {
-  if (e.key == "Enter") {
+  const key = e.key;
+  if (key === "Enter") {
     equal();
-  } else if (e.key == "Backspace") {
+  } else if (key === "Backspace") {
     erase();
-  }else if(e.key == "/"){
-    divideOperator();
-  } else if (e.key == "+") {
-    plusOperator();
-  } else if (e.key == "-") {
-    minusOperator();
-  } else if (e.key == "*") {
-    multipleOperator();
-  } else if (Number(e.key) || e.key == "." || e.key == "(" || e.key == ")") {
+  } else if (["/", "+", "-", "*"].includes(key)) {
+    applyOperator(key);
+  } else if (Number(key) || key === "." || key === "(" || key === ")") {
     if (continuez) {
-        mainSrt = "0";
-        continuez = false;
-      }
-    if (e.key != "( )") mainSrt += e.key;
-    if (mainSrt[0] == "0" && mainSrt[1] != ".") {
-      mainSrt = mainSrt.slice(1);
+      mainStr = "0";
+      continuez = false;
     }
-    document.querySelector("#sum").innerHTML = mainSrt;
+    if (key !== "( )") mainStr += key;
+    if (mainStr[0] === "0" && mainStr[1] !== ".") {
+      mainStr = mainStr.slice(1);
+    }
+    document.querySelector("#sum").innerHTML = mainStr;
   }
 });
